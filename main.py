6c592a973b5c8d496d4e6c69d65f92c1d1b365a4
Takumi Kumagai <kumagai.takumi1223@gmail.com>
2020-08-16 14:53:07 +0900

    refactored: organized into Arm and Arms classes.

--- main.py
+++ main.py
@@ -3,64 +3,73 @@ import numpy
 import scipy
 from scipy.stats import bernoulli
 
-class Results:
-    class Result_Arm:
-        def __init__(self):
-            self.success = 0
-            self.failure = 0
+class Arm:
+    def __init__(self,ctr):
+        self.success = 0
+        self.failure = 0
+        self.ctr = ctr
+        
+    def add_success(self):
+        self.success += 1
 
-    def __init__(self,arm_count):
-        self._results = [self.Result_Arm() for i in range(arm_count)]
+    def add_failure(self):
+        self.failure += 1
+
+    def get_success_count(self):
+        return self.success
+
+    def get_failure_count(self):
+        return self.failure
+
+class Arms:
+    def __init__(self,ctrs_true):
+        self.arms = [Arm(ctr) for ctr in ctrs_true]
+        self.total_success = 0
     
-    def add_success(self,arm_index):
-        self._results[arm_index].success += 1
+    def _sample_from_beta(self,success_count,failure_count):
+        #prior distribution is uniform: a = b = 1
+        alpha = 1 + success_count
+        beta = 1 + failure_count
+        return numpy.random.beta(alpha, beta)
 
-    def add_failure(self,arm_index):
-        self._results[arm_index].failure += 1
+    def choose_arm_to_pull(self):
+        ctrs_sampled = []
+        for arm_index in range(len(self.arms)):
+            success_count = self.arms[arm_index].get_success_count()
+            failure_count = self.arms[arm_index].get_failure_count()
+            ctrs_sampled.append(self._sample_from_beta(success_count,failure_count))
+        return numpy.argmax(ctrs_sampled)
 
-    def get_success_count(self,arm_index):
-        return self._results[arm_index].success
-
-    def get_failure_count(self,arm_index):
-        return self._results[arm_index].failure
+    def pull_and_record_outcome(self,chosen_arm_index):
+        p = self.arms[chosen_arm_index].ctr
+        outcome = bernoulli.rvs(p)
+        if outcome == 0: #failure
+            self.arms[chosen_arm_index].add_failure()
+        else: #success
+            self.total_success+=1
+            self.arms[chosen_arm_index].add_success()
 
 
 if __name__ == "__main__":
+    #number of trials
     time_count = 100000
-    arm_count = 100
-    alpha = 1
-    beta = 1
-
-    #to be a list of bernoulli districutions, which model newly repleased books
-    arms = [] 
-
+    
     #to be the list of parameters for the bernoulli distributions
     ctrs_true = []
 
-    for i in range(arm_count):
+    for i in range(100):
         # set a random ctr less than 10%
         ctr_true = random.random()/10
         ctrs_true.append(ctr_true)
-        
-    results = Results(arm_count)
-
-    hit = 0
-
+    arms = Arms(ctrs_true)
+    
     for i in range(time_count):
         if i % 10000 == 0:
             print("completed:"+str(i)+"/"+str(time_count))
         ctrs_sampled = []
-        for j in range(arm_count):
-            ctrs_sampled.append(numpy.random.beta(alpha + results.get_success_count(j), beta + results.get_failure_count(j)))
-        arm_to_pull = numpy.argmax(ctrs_sampled)
-        outcome = bernoulli.rvs(ctrs_true[arm_to_pull])
-        if outcome == 0:
-            results.add_failure(arm_to_pull)
-        else:
-            hit += 1
-            results.add_success(arm_to_pull)
+        arm_to_pull = arms.choose_arm_to_pull()
+        arms.pull_and_record_outcome(arm_to_pull)
 
-    print("outcome ctr:" + str(hit/time_count))
+    print("outcome ctr:" + str(arms.total_success/time_count))
     print("true ctrs:"+ str(ctrs_true))
-    print("true ctrs mean:"+ str(sum(ctrs_true)/arm_count))
-
+    print("true ctrs mean:"+ str(sum(ctrs_true)/len(arms.arms)))
